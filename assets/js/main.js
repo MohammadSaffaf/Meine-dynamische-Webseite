03c2f44c783ffd9763cd9cf9f27e035567c2e591
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-11-10 10:34:44 +0100

    javascribt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,34 @@
-let result=document.querySelector('body')
-let rosa = document.getElementById('rosa')
-let lila = document.getElementById('lila')
-let orange = document.getElementById('orange')
-let demo = document.getElementById('demo')
-const red = document.getElementById("rot")
-const green = document.getElementById("grun")
-const blue = document.getElementById("blau")
+// let result=document.querySelector('body')
+// let rosa = document.getElementById('rosa')
+// let lila = document.getElementById('lila')
+// let orange = document.getElementById('orange')
+// let demo = document.getElementById('demo')
+// const red = document.getElementById("rot")
+// const green = document.getElementById("grun")
+// const blue = document.getElementById("blau")
 // let count = 0
+
+
+// function andernHintergrund(color) {
+//     count ++
+//     result.style.background = color
+//     demo.innerHTML = `Wir haben die Farbe ${count} mal geändert`
+// }
+
+
+
+// function andernRegler() {
+    
+//     result.style.background = `rgb(${red.value},${green.value},${blue.value})`
+//     demo.innerHTML = `rgb(${red.value},${green.value},${blue.value}) = ${rgbToHex(+red.value, +green.value, +blue.value)}`
+// }
+// const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+//     const hex = x.toString(16)
+//     return hex.length === 1 ? '0' + hex : hex
+// }).join('')
+
+
+// andere möglichkeit für button________________________
 
 // rosa.addEventListener('click', () => result.style.background="pink")
 // rosa.addEventListener('click', () => demo.innerHTML="Wir haben die farbe "+ 3 +" geändert")
@@ -14,21 +36,29 @@ const blue = document.getElementById("blau")
 // lila.addEventListener('click', () => demo.innerHTML="Wir haben die farbe "+ 4 +" geändert")
 // orange.addEventListener('click', () => result.style.background="orange")
 // orange.addEventListener('click', () => demo.innerHTML="Wir haben die farbe "+ 5 +" geändert")
-function andernHintergrund(color) {
-    
-    result.style.background = color
-    demo.innerHTML = `Wir haben die Farbe ${count} mal geändert`
+
+
+// GEORG die corector___________
+let count = 0
+const andernHintergrund = (farbe) => {
+    count++
+    document.querySelector('body').style.background= farbe
+    document.getElementById('demo').innerHTML= `Wir haben die Farbe ${count} mal geändert`
 }
-
-
-
-function andernRegler() {
-    
-    result.style.background = `rgb(${red.value},${green.value},${blue.value})`
-    demo.innerHTML = `rgb(${red.value},${green.value},${blue.value}) = ${rgbToHex(+red.value, +green.value, +blue.value)}`
+let red = 255
+let green = 255
+let blue = 255
+const andernRegler = () => {
+    if (event.target.id === "rot") {
+        red = event.target.value
+    } else if (event.target.id === "grun") {
+        green = event.target.value
+    } else if (event.target.id === "blau") {
+        blue = event.target.value
+    }
+    // console.log("rot:", red, "Grün:", green, "Blau", blue);
+    const farbe = `rgb(${red},${green},${blue})`
+    // console.log(farbe);
+    document.querySelector('body').style.background = farbe
+    document.getElementById('demo').innerHTML = farbe + ", #" + (red * 1).toString(16) + (green * 1).toString(16) + (blue * 1).toString(16)
 }
-const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
-    const hex = x.toString(16)
-    return hex.length === 1 ? '0' + hex : hex
-}).join('')
-
